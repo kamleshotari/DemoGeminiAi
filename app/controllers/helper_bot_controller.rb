da5c2b1a2cab4006c31274c3e7bf23d6efe32b42
kamleshotari <kamleshotari@gmail.com>
2024-01-03 01:33:59 +0530

    fix env read issue

--- app/controllers/helper_bot_controller.rb
+++ app/controllers/helper_bot_controller.rb
@@ -1,7 +1,7 @@
 class HelperBotController < ApplicationController
   def index
     @result = ''
-    bot = NanoBot.new(cartridge: 'cartridge.yml')
+    bot = NanoBot.new(cartridge:  YAML.safe_load(ERB.new(File.read('cartridge.yml')).result))
     @result = bot.eval(params[:question], as: 'repl') if params[:question].present?
     respond_to do |format|
       format.html
