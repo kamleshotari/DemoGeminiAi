75c611dfe22ba6c9257a62e24563178d012b6b7e
Shobhit <shobits@Shobhits-MacBook-Air.local>
2024-01-17 18:32:15 +0530

    adding widget

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   get '/helper_bot', to: 'helper_bot#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :widget, only: :index
+
+  root 'home#index'
 end
